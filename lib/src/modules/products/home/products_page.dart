65fab45476d56fbbc0b55d507226143e64b31304
Eduardo Pereira <eduardo.souzapereira@gmail.com>
2023-05-18 22:45:40 -0300

    refactory

--- lib/src/modules/products/home/products_page.dart
+++ lib/src/modules/products/home/products_page.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/ui/widgets/base_header.dart';
+
 class ProductsPage extends StatelessWidget {
   const ProductsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Products'),
+    return Container(
+      color: Colors.grey[50],
+      padding: const EdgeInsets.only(left: 40, top: 40, right: 40),
+      child: Column(
+        children: [
+          BaseHeader(
+            title: 'ADMINISTRAR PRODUTOS',
+            buttonLabel: 'ADICIONAR PRODUTO',
+            buttonPressed: () {},
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Expanded(
+            child: GridView.builder(
+              itemCount: 10,
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                mainAxisExtent: 280,
+                mainAxisSpacing: 20,
+                maxCrossAxisExtent: 280,
+                crossAxisSpacing: 10,
+              ),
+              itemBuilder: (context, index) {
+                return Text('Produto $index');
+              },
+            ),
+          ),
+        ],
       ),
-      body: Container(),
     );
   }
 }
